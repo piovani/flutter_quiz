60f4acb64640a70f3c2cca0820ebb7832a2cb01a
Allison Piovani <apiovani25@gmail.com>
2020-05-21 20:48:10 -0300

    Init events

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,45 @@ main() {
 
 class PerguntaApp extends StatelessWidget {
 
+  void responder() {
+    print('Pergunta respondida');
+  }
+
+  void Function() funcaoQueRetornaUmaOutraFuncao() {
+    return () {
+      print('Pergunta respondida 2');
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Ola Flutter'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed:  responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed:  funcaoQueRetornaUmaOutraFuncao(),
+            ),
+          ],
+        ),
       ),
     );
   }
