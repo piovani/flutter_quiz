c814cd07022196274b6c4d5f6c2a0e940410ab05
Allison Piovani <apiovani25@gmail.com>
2020-06-01 22:29:20 -0300

    Add points in questions

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,30 @@ class _PerguntaAppState extends State<PerguntaApp> {
   final List<Map<String, Object>> _perguntas = const [
     {
       'texto': 'Qual é a sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Azul', 'Branco']
+      'respostas': [
+        {'texto': 'Preto', 'nota': 10},
+        {'texto': 'Vermelho', 'nota': 5},
+        {'texto': 'Azul', 'nota': 3},
+        {'texto': 'Branco', 'nota': 1},
+      ]
     },
     {
       'texto': 'Qual é o seu animal favorito?',
-      'respostas': ['Cachorro', 'Coelho', 'Gato', 'Leão']
+      'respostas': [
+        {'texto': 'Cachorro', 'nota': 10},
+        {'texto': 'Coelho', 'nota': 5},
+        {'texto': 'Gato', 'nota': 3},
+        {'texto': 'Leão', 'nota': 1},
+      ]
     },
     {
       'texto': 'Qual é a sua comida favorita?',
-      'respostas': ['Macarrão', 'Pizza', 'Lanche', 'Panqueca']
+      'respostas': [
+        {'texto': 'Macarrão', 'nota': 10},
+        {'texto': 'Pizza', 'nota': 5},
+        {'texto': 'Lanche', 'nota': 3},
+        {'texto': 'Panqueca','nota': 1},
+      ]
     }
   ];
 
@@ -37,31 +52,27 @@ class _PerguntaAppState extends State<PerguntaApp> {
     return _perguntaSelecionada < _perguntas.length;
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
-    
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: temPerguntaSelecionada 
-          ? Questionario(
-              perguntas: _perguntas,
-              perguntaSelecionada: _perguntaSelecionada,
-              quandoResponder: _responder,
-            )
-          : Resultado(),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder,
+              )
+            : Resultado(),
       ),
     );
   }
 }
 
 class PerguntaApp extends StatefulWidget {
-
   _PerguntaAppState createState() {
     return new _PerguntaAppState();
   }
-
 }
