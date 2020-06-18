daed46cbc0a1b6bbce230a106dda3a270043f0d1
Allison Piovani <apiovani25@gmail.com>
2020-06-18 20:17:36 -0300

    Add final message

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quandoResponder: _responder,
               )
-            : Resultado(),
+            : Resultado(this._pontuacaoTotal),
       ),
     );
   }
